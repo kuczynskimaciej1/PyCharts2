f90d4fe5cef291475652c3dfdec809a084405920
Maciej Kuczynski <kuczynskimaciej1@poczta.onet.pl>
2024-10-27 19:24:51 +0100

    New files added

---
+++ src/menu.py
@@ -0,0 +1,13 @@
+import globals
+import data
+
+def start():
+    print("PyCharts v2.0 - the dynamic AI learning analysis software")
+    print("Created by Maciej Kuczynski, 2024/2025")
+    #data.fix_parentheses()
+    data.read_file()
+    #while(True):
+    #    menu()
+
+def menu():
+    pass
