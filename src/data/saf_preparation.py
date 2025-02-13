f1420aabd163f85eb8170ff58b5be27fcf9a7547
Maciej Kuczyński <kuczynskimaciej1@poczta.onet.pl>
2025-02-13 13:27:13 +0100

    SAF duplicate drop fixed

--- src/data/saf_preparation.py
+++ src/data/saf_preparation.py
@@ -18,7 +18,7 @@ def prepare_data() -> None:
     november2018 = pd.read_csv("../../data/Spotify Audio Features/original/SpotifyAudioFeaturesNov2018.csv")
     april2019 = pd.read_csv("../../data/Spotify Audio Features/original/SpotifyAudioFeaturesApril2019.csv")
     total_saf = pd.concat([november2018, april2019], ignore_index = True)
-    total_saf.drop_duplicates(subset=['track_id'])
+    total_saf = total_saf.drop_duplicates(subset=['track_id'])
 
     # Zmień wartości w kolumnie artist_name na listy stringów
     def convert_to_list(artist_name):
