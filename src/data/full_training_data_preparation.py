f01fd39b9a805d02758e4348de82865f250e74e6
Maciej Kuczyński <kuczynskimaciej1@poczta.onet.pl>
2025-02-13 14:49:13 +0100

    SAF function 'all' added

--- src/data/full_training_data_preparation.py
+++ src/data/full_training_data_preparation.py
@@ -28,3 +28,14 @@ def delete_cols() -> None:
     total: pd.DataFrame = pd.read_csv('../../data/full_training_data.csv')
     total = total.drop(columns=['Unnamed: 0.3','Unnamed: 0.2','Unnamed: 0.1','Unnamed: 0'])
     total.to_csv('../../data/full_training_data.csv')
+
+
+def all():
+    set_8m: pd.DataFrame = pd.read_csv('../../data/8+ M. Spotify Tracks, Genre, Audio Features/8m_data.csv')
+    set_10m: pd.DataFrame = pd.read_csv('../../data/10+ M. Beatport Tracks/10m_data.csv')
+    total: pd.DataFrame = pd.concat([set_8m, set_10m], ignore_index = True, axis=0)
+    total = total.drop_duplicates(subset=['track_id'], ignore_index=True)
+    print(total.shape[0])
+    print(total['track_id'].nunique())
+    print(total.columns)
+    total.to_csv('../../data/full_training_data.csv')
